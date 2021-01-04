e9562941efb805ffbddc917d9660a2c9568b9f0f
tinalenguyen <>
2021-01-04 18:53:35 -0500

    working more

--- MakeStars.java
+++ MakeStars.java
@@ -4,20 +4,20 @@ public class MakeStars {
   public static void main(String[] args){
     Scanner obj = new Scanner(System.in);
 
-    String result = " ";
+    String thing = " ";
 
     while (obj.hasNextLine() ){
         Scanner readLine = new Scanner(obj.nextLine() );
         while (readLine.hasNext()){
-          for (int i = 0 ; i < readLine.next().length() ; i++ ){
+          String result = " ";
+          thing = readLine.next();
+          for (int i = 0 ; i < thing.length() ; i++ ){
             result += "*";
 
           }
           System.out.println(result + " ");
         }
-        System.out.println();
     }
-    obj.close();
 
 
   }
