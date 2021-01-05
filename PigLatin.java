3192ab2c558d2c6560ca66253b3267229e8004a9
tinalenguyen <>
2021-01-04 23:16:09 -0500

    final version, tested sonnet

--- PigLatin.java
+++ PigLatin.java
@@ -27,13 +27,18 @@ public class PigLatin {
       boolean containDigraph = false;
 
       String[] digraphs = {"bl", "br", "ch", "ck", "cl", "cr", "dr", "fl", "fr", "gh", "gl", "gr", "ng", "ph", "pl", "pr", "qu", "sc", "sh", "sk", "sl", "sm", "sn", "sp", "st", "sw", "th", "tr", "tw", "wh", "wr"};
-      for (int i = 0 ; i < digraphs.length ; i++){
-        if (s.contains(digraphs[i] ) ) containDigraph = true;
+      if (s.length()>1 ){
+       for (int i = 0 ; i < digraphs.length ; i++){
+        if (s.substring(0, 2).contains(digraphs[i] ) ) {
+          containDigraph = true;
+          i = digraphs.length;
+        }
       }
+    }
       if (s.charAt(0) == 'a' || s.charAt(0) == 'e' || s.charAt(0) == 'u' ||
           s.charAt(0) == 'i' || s.charAt(0) == 'o' ){
             return s +"hay";
-      } else if (containDigraph == true){
+      } else if (containDigraph==true){
         return s.substring(2) + s.substring(0,2) +"ay";
     } else return s.substring(1) + s.charAt(0) +"ay";
 
@@ -46,7 +51,7 @@ public class PigLatin {
       if (!Character.isLetter(s.charAt(0)) ) return s;
       //accounts for punctuation
       else if (!Character.isLetterOrDigit(lastChar) ){
-        return pigLatin(s) + lastChar;
+        return pigLatin(s.substring(0, s.length() -1) ) + lastChar;
       }
       else return pigLatin(s);
 
