b22e3919b8b437e5794de9561a799f19cbb05584
tinalenguyen <>
2021-01-04 19:22:29 -0500

    oadded piglatinsimple

--- PigLatin.java
+++ PigLatin.java
@@ -5,7 +5,11 @@ public class PigLatin {
     }
 
     public static String pigLatinSimple(String s){
-
+      s = s.toLowerCase();
+      if (s.charAt(0) == 'a' || s.charAt(0) == 'e' || s.charAt(0) == 'u'
+          s.charAt(0) == 'i' || s.charAt(0) == 'o' ){
+            return s +"hay";
+          } else return s.substring(1) + s.charAt(0) +"ay";
 
 
     }
@@ -19,7 +23,7 @@ public class PigLatin {
 
     public static String pigLatinBest(String s){
 
-    
+
     }
 
 
