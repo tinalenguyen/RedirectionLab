94e27b0bc74f5c05de697b65116c6fdfdb134d6d
tinalenguyen <>
2021-01-04 19:05:53 -0500

    outline

---
+++ PigLatin.java
@@ -0,0 +1,27 @@
+public class PigLatin {
+
+    public static void main(String[] main){
+
+    }
+
+    public static String pigLatinSimple(String s){
+
+
+
+    }
+
+
+    public static String pigLatin(String s){
+
+
+
+    }
+
+    public static String pigLatinBest(String s){
+
+    
+    }
+
+
+
+}
